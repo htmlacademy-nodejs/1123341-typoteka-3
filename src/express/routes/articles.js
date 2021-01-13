0fe7252b5653c4255ad7fdeae36dad2654ab4459
tyort <neotyo1986@gmail.com>
2021-01-14 01:57:25 +0300

    pug

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`publications-by-category`));
+articlesRouter.get(`/add`, (req, res) => res.render(`admin-add-new-post-empty`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`admin-add-new-post`)); // ??????
+articlesRouter.get(`/:id`, (req, res) => res.render(`post-user`));
 
 module.exports = articlesRouter;
--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`));
-myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRouter.get(`/`, (req, res) => res.send(`admin-publications`));
+myRouter.get(`/comments`, (req, res) => res.send(`admin-comments`));
 
 module.exports = myRouter;
