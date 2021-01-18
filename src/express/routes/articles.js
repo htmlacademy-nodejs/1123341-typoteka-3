0c4c8a97c7495d3181aaaebf9e4200dc65a5e21e
tyort <neotyo1986@gmail.com>
2021-01-18 12:50:16 +0300

    рефакторинг путей pug

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -4,8 +4,8 @@ const {Router} = require(`express`);
 const articlesRouter = new Router();
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`publications-by-category`));
-articlesRouter.get(`/add`, (req, res) => res.render(`admin-add-new-post-empty`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`admin-add-new-post`)); // ??????
-articlesRouter.get(`/:id`, (req, res) => res.render(`post-user`));
+articlesRouter.get(`/add`, (req, res) => res.render(`./admin/admin-add-new-post-empty`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`.admin/admin-add-new-post`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`./post/post-user`));
 
 module.exports = articlesRouter;
--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -3,10 +3,10 @@
 const {Router} = require(`express`);
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, (req, res) => res.render(`./main/main`));
 mainRouter.get(`/register`, (req, res) => res.render(`registration`));
 mainRouter.get(`/login`, (req, res) => res.render(`registration`));
-mainRouter.get(`/search`, (req, res) => res.render(`search-1`));
-mainRouter.get(`/categories`, (req, res) => res.render(`admin-categories`));
+mainRouter.get(`/search`, (req, res) => res.render(`./search/search-1`));
+mainRouter.get(`/categories`, (req, res) => res.render(`./admin/admin-categories`));
 
 module.exports = mainRouter;
