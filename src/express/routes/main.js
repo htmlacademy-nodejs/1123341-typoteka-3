93063454330736c3423d722e673f364152228b78
tyort <neotyo1986@gmail.com>
2021-02-15 02:59:47 +0300

    поиск pug

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -4,7 +4,6 @@ const {Router} = require(`express`);
 const mainRouter = new Router();
 const api = require(`../api`).getAPI();
 
-// Главная страница
 mainRouter.get(`/`, async (req, res) => {
   const [articles, categories] = await Promise.all([
     api.getArticles(),
@@ -20,10 +19,25 @@ mainRouter.get(`/`, async (req, res) => {
   res.render(`./main/main`, {articles, categories, categoriesCapacity});
 });
 
+mainRouter.get(`/search`, async (req, res) => {
+
+  try {
+    const {search} = req.query;
+    const results = await api.search(search);
+    res.render(`search/search-2`, {
+      results,
+      searchText: search
+    });
+
+  } catch (error) {
+    res.render(`search/search-2`, {
+      results: []
+    });
+  }
+});
 
 mainRouter.get(`/register`, (req, res) => res.render(`registration`));
 mainRouter.get(`/login`, (req, res) => res.render(`registration`));
-mainRouter.get(`/search`, (req, res) => res.render(`./search/search-1`));
 mainRouter.get(`/categories`, (req, res) => res.render(`./admin/admin-categories`));
 
 module.exports = mainRouter;
