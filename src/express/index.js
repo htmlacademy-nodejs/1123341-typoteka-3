168ca2d8919833353183e3aedd3b2326cf069346
tyort <neotyo1986@gmail.com>
2021-04-24 15:46:52 +0300

    express-session

--- src/express/index.js
+++ src/express/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const express = require(`express`);
+const expressSession = require(`express-session`);
+const {secretSession, sequelizeStore} = require(`../service/lib/session-store`);
 const path = require(`path`);
 const articlesRoutes = require(`./routes/articles`);
 const myRoutes = require(`./routes/my`);
 const mainRoutes = require(`./routes/main`);
+const authRoutes = require(`./routes/auth`);
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
@@ -12,9 +15,18 @@ const UPLOAD_DIR = `upload`;
 
 const app = express();
 
+app.use(expressSession({
+  store: sequelizeStore,
+  secret: secretSession,
+  resave: false,
+  saveUninitialized: false,
+  name: `session_id`
+}));
+
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+app.use(`/`, authRoutes);
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
