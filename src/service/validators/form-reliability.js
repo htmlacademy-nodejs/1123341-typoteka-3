f7a534ab82e64123f1c604bb188afe04e6a049fc
tyort <neotyo1986@gmail.com>
2021-05-06 15:00:30 +0300

    Корректировка form-reliability

--- src/service/validators/form-reliability.js
+++ src/service/validators/form-reliability.js
@@ -4,7 +4,8 @@ const bcrypt = require(`bcrypt`);
 const {DB_FORM_RELIABILITY} = process.env;
 
 module.exports = async (req, res, next) => {
-  const match = await bcrypt.compare(DB_FORM_RELIABILITY, req.body[`user-csrf`]);
+  console.log(req.body[`member-csrf`]);
+  const match = await bcrypt.compare(DB_FORM_RELIABILITY, req.body[`member-csrf`]);
 
   if (!match) {
     res.redirect(`/login`);
