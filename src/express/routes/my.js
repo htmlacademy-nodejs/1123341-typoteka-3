0c64b7f92de28e34403301b2dceb5cae6954b8d1
tyort <neotyo1986@gmail.com>
2021-05-10 22:10:22 +0300

    Рефакторинг http://localhost:8080/my/comments

--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const dayjs = require(`dayjs`);
+const jwt = require(`jsonwebtoken`);
 const {Router} = require(`express`);
 const {compareDate} = require(`../../utils`);
 const myRouter = new Router();
 const api = require(`../api`).getAPI();
+const authenticateJwt = require(`../../service/validators/authenticate-jwt`);
+
+const {JWT_ACCESS_SECRET} = process.env;
 
 myRouter.get(`/`, async (req, res) => {
   let articles = await api.getArticles();
@@ -12,13 +16,23 @@ myRouter.get(`/`, async (req, res) => {
   res.render(`./admin/admin-publications`, {articles, dayjs});
 });
 
-myRouter.get(`/comments`, async (req, res) => {
+myRouter.get(`/comments`, authenticateJwt, async (req, res) => {
+  const token = req.cookies[`authorization`];
+  const userData = jwt.verify(token, JWT_ACCESS_SECRET);
+
   const articles = await api.getArticles({comments: true});
   const sortedComments = articles
     .flatMap((article) => article.comments)
     .sort(compareDate);
 
-  res.render(`./admin/admin-comments`, {sortedComments});
+  res.render(`./admin/admin-comments`, {
+    dayjs,
+    sortedComments,
+    isLogged: userData.isLogged,
+    userAvatar: userData.userAvatar,
+    userName: userData.userName,
+    userSurname: userData.userSurname
+  });
 });
 
 module.exports = myRouter;
